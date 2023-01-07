f12016d370cacd31cffbdb17c9e1a9cc9539582a
khaoula <kaoulatouati@gmail.com>
2023-01-07 16:35:38 +0100

    mongo db cnx + config

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,12 +2,50 @@
 
 namespace App\Controller;
 
+use App\Document\Produit;
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
 {
+
+
+    /**
+     * @Route("/affiche")
+     */
+    public function affiche(DocumentManager $documentManager)
+    {
+        $user = $documentManager->getRepository(Produit::class)->findAll();
+        return $this->render("affiche.html.twig",[
+            "produit"=>$user
+        ]);
+    }
+
+    /**
+     * @Route("/ajout", methods={"GET"})
+     */
+    public function ajouter(DocumentManager $dm)
+    {
+
+        $produit = new Produit();
+        $produit->setNom("Smartphone");
+        $produit->setMarque("Apple");
+        $produit->setQte("150");
+//        $produit->setDateFab("15/02/2023");
+        $produit->setPrix(6500);
+
+
+        $dm->persist($produit);
+        $dm->flush();
+//        return new JsonResponse(['Status' => 'OK']);
+
+        return new Response("ok :)");
+    }
+
+
     /**
      * @Route("/", name="home")
      */
