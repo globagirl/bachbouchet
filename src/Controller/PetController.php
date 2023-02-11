1c390dccc20c810a027a831155cf752cd44b640b
khaoula <kaoulatouati@gmail.com>
2023-02-11 21:57:52 +0100

    pets

--- src/Controller/PetController.php
+++ src/Controller/PetController.php
@@ -7,7 +7,10 @@
  */
 
 namespace App\Controller;
+use App\Document\Pet;
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 /**
@@ -19,17 +22,34 @@ class PetController extends AbstractController
     /**
      * @Route("/", name="pets-list")
      */
-    public function index(): Response
+    public function index(DocumentManager $dm): Response
     {
         return $this->render('Account/pets/list_pets.html.twig');
     }
 
     /**
      * @Route("/ajout", name="pets-add")
+     * @throws \Doctrine\ODM\MongoDB\MongoDBException
      */
-    public function ajout(): Response
+    public function ajout(Request $request, DocumentManager $dm)
     {
-        return $this->render('Account/pets/ajout_pet.html.twig');
+        $pet=new Pet();
+        $form = $this->createForm('App\Form\PetType', $pet);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+//            var_dump($pet);
+            $dm->persist($pet);
+            $dm->flush();
+
+                $this->addFlash(
+                    'notice',
+                    'Pet added successfully ! '
+                );
+
+            return $this->redirect($this->generateUrl('pets-list'));
+        }
+        return $this->render('Account/pets/ajout_pet.html.twig', ['form' => $form->createView()]);
     }
 
     /**
